0847427596c21f2dcd112d1a2ac072ce8b53e10b
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-07-11 11:47:06 +0300

    Task1done

---
+++ NET.S.2018,Drachylouski.07_08/Task1Logic/Customer.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task1Logic
+{
+
+    /// <summary>
+    /// Custom class
+    /// </summary>
+    /// <seealso cref="System.IFormattable" />
+    public sealed class Customer : IFormattable
+    {        
+        private static readonly string patternPhoneNumber = @"\((?<AreaCode>\d{3})\)\s*(?<Number>\d{3}(?:-|\s*)\d{4})";
+        private static readonly string patternName = @"^[\p{L}\p{M}' \.\-]+$";
+
+        private string name;
+        private string contactPhone;
+        private decimal revenue;
+
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                ValidateOfRegularExpressions(patternName, value);
+
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the contact phone.
+        /// </summary>
+        /// <value>
+        /// The contact phone.
+        /// </value>
+        public string ContactPhone
+        {
+            get => contactPhone;
+
+            set
+            {
+                ValidateOfRegularExpressions(patternPhoneNumber, value);
+
+                contactPhone = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the revenue.
+        /// </summary>
+        /// <value>
+        /// The revenue.
+        /// </value>
+        /// <exception cref="ArgumentOutOfRangeException">revenue</exception>
+        public decimal Revenue
+        {
+            get => revenue;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(revenue)} can't be negative");
+                }
+
+                revenue = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="formatProvider">The format provider.</param>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            string temp = format.ToUpperInvariant();
+
+            string result = string.Empty;
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                switch (temp[i])
+                {
+                    case 'N':
+                    {
+                        result += Name.ToString(formatProvider);
+                        break;
+                    }
+                    case 'P':
+                    {
+                        result += ContactPhone.ToString(formatProvider);
+                        break;
+                    }
+                    case 'R':
+                    {
+                        result += Revenue.ToString(formatProvider);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return Name + ContactPhone + Revenue.ToString("G");
+        }
+
+        /// <summary>
+        /// Validates the of regular expressions.
+        /// </summary>
+        /// <param name="pattern">The pattern.</param>
+        /// <param name="value">The value.</param>
+        /// <exception cref="ArgumentException">value</exception>
+        private static void ValidateOfRegularExpressions(string pattern, string value)
+        {
+            Regex regex = new Regex(pattern);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new ArgumentException(nameof(value));
+            }
+        }
+    }
+}
---
+++ NET.S.2018,Drachylouski.07_08/Task1Test/Task1Test.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using Task1Logic;
+using System.Threading;
+
+namespace Task1Test
+{
+
+    [TestFixture]
+    public class Task1Test
+    {
+        [TestCase("Vladimir Putin", "+1 (354) 975-1188", 99999999)]
+        [TestCase("Anton Drachylouski", "+37 (525) 775-7834", 10000000)]
+        [TestCase("Vova Sidorov", "+7 (926) 123-4567",150)]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 14141414)]
+        public void TestCase(string name, string number, decimal money)
+        {
+            Customer obj = new Customer(name,number,money);
+
+            string actual = obj.ToString("NPR", CultureInfo.InvariantCulture);
+
+            string expected = obj.ToString();
+
+            Assert.True(actual==expected);
+        }
+
+        [TestCase("Vladimir Putin", "+1 (354) 975-1188", 99999999)]
+        [TestCase("Anton Drachylouski", "+37 (525) 775-7834", 10000000)]
+        [TestCase("Vova Sidorov", "+7 (926) 123-4567", 150)]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 14141414)]
+        public void TestCase2(string name, string number, decimal money)
+        {
+            Customer obj = new Customer(name, number, money);
+
+            string actual = obj.ToString("NP", CultureInfo.InvariantCulture);
+
+            string expected = $"{obj.Name}{obj.ContactPhone}";
+
+            Assert.True(actual == expected);
+        }
+
+        [TestCase("Vladimir Putin", "+1 (354) 975-1188", 99999999)]
+        [TestCase("Anton Drachylouski", "+37 (525) 775-7834", 10000000)]
+        [TestCase("Vova Sidorov", "+7 (926) 123-4567", 150)]
+        [TestCase("Jeffrey Richter", "+1 (425) 555-0100", 14141414)]
+        public void TestCase3(string name, string number, decimal money)
+        {
+            Customer obj = new Customer(name, number, money);
+
+            string actual = obj.ToString("R", CultureInfo.InvariantCulture);
+
+            string expected = $"{obj.Revenue}";
+
+            Assert.True(actual == expected);
+        }
+
+        [TestCase("dakdk1k 999dzm", "+1 (354) 975-1188", 99999999)]
+        [TestCase("Anton Drachylouski", "adadadadadada", 10000000)]
+        public void TestCaseException(string name, string number, decimal money)
+        {
+            Assert.Throws<ArgumentException>(() => new Customer(name, number, money));
+        }
+
+
+
+    }
+}
