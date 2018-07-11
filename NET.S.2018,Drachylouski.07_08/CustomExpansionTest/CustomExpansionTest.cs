6976e87a967a66f47444ed89a8e707c6cd4025fe
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-07-11 13:21:06 +0300

    done

---
+++ NET.S.2018,Drachylouski.07_08/CustomExpansionTest/CustomExpansionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using CustomerExpansion;
+using Task1Logic;
+
+namespace CustomExpansionTest
+{
+    [TestFixture]
+    public class CustomExpansionTest
+    {
+        [TestCase("Vladimir Putin", "+1 (354) 975-1188", 99999999,"W")]
+        public void TestCase1(string name, string number, decimal money,string format)
+        {
+            IFormatProvider formatProvider = CultureInfo.CurrentCulture;
+            Customer customer = new Customer(name, number, money);
+            var customerExpansion = new CustomerExpansion.CustomerExpansion();
+
+            string actual = customerExpansion.Format(format, customer, formatProvider);
+            string expected = "plus one three five four nine seven five one one eight eight";
+            Assert.True(actual==expected);
+        }
+    }
+}
---
+++ NET.S.2018,Drachylouski.07_08/CustomerExpansion/CustomerExpansion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1Logic;
+
+namespace CustomerExpansion
+{
+    /// <summary>
+    /// Expansion for customer class
+    /// </summary>
+    /// <seealso cref="System.ICustomFormatter" />
+    /// <seealso cref="System.IFormatProvider" />
+    public sealed class CustomerExpansion : ICustomFormatter, IFormatProvider
+    {
+        private static readonly string[] numberWords =
+            "zero one two three four five six seven eight nine plus".Split();
+
+        public IFormatProvider Parent { get; private set; }
+
+        public CustomerExpansion() : this(CultureInfo.CurrentCulture)
+        {
+
+        }
+
+        public CustomerExpansion(IFormatProvider parent)
+        {
+            Parent = parent ?? CultureInfo.CurrentCulture;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null || format != "W")
+                return string.Format(Parent, "{0:" + format + "}", arg);
+
+            StringBuilder result = new StringBuilder();
+
+            var customer = arg as Customer;
+
+            if (!ReferenceEquals(customer,null))
+            {
+                string digitList = string.Format(CultureInfo.InvariantCulture, "{0}", customer.ContactPhone);
+                foreach (char digit in digitList)
+                {
+                    int i = "0123456789+".IndexOf(digit);
+                    if (i == -1) continue;
+                    if (result.Length > 0) result.Append(' ');
+                    result.Append(numberWords[i]);
+                }
+
+                return result.ToString();
+            }
+
+            return arg.ToString();
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+    }
+}
